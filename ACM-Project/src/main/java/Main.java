ba475ef1b1f5847e6c852e1c20bc048be3fea193
Matthew Dutchess <55841244+indydutch@users.noreply.github.com>
2021-11-09 20:42:24 -0500

    Delete ACM-Project directory

--- ACM-Project/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.security.auth.login.LoginException;
-
-public class Main {
-    public static void main(String[] args) throws LoginException // main function for the command detection
-    {
-        System.out.println("Hello world!");
-    }
-}
