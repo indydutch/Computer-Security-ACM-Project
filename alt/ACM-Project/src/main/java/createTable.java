a17514dd05d10bc02f5d15e95019f5143fc25392
Matthew Dutchess <55841244+indydutch@users.noreply.github.com>
2021-11-11 00:33:24 -0500

    Delete alt/ACM-Project directory

--- alt/ACM-Project/src/main/java/createTable.java
+++
@@ -1,6 +0,0 @@
-public class createTable {
-    public static void main(String[] args) // main function for the command detection
-    {
-        TableCreation testObj = new TableCreation();
-    }
-}
