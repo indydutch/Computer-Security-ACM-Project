77036e522216fe16c29953bdb1eeb035132d0cdf
Matthew Dutchess <55841244+indydutch@users.noreply.github.com>
2021-11-10 21:06:52 -0500

    Add files via upload

---
+++ alt/ACM-Project/src/main/java/createTable.java
@@ -0,0 +1,6 @@
+public class createTable {
+    public static void main(String[] args) // main function for the command detection
+    {
+        TableCreation testObj = new TableCreation();
+    }
+}
---
+++ alt/ACM-Project/src/main/java/deleteTable.java
@@ -0,0 +1,14 @@
+public class deleteTable {
+    public static void main(String[] args) // main function for the command detection
+    {
+        TableCreation testObj = new TableCreation();
+
+        /*
+        testObj.createSubject("s1");
+        testObj.createSubject("s2");
+        testObj.createSubject("s3");
+         */
+
+        testObj.deleteTable();
+    }
+}
